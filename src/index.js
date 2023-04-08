bd2a27b9c5b2c81967127922525c834b0d8488ef
Andrei Moldovan <andreimoldovan6@gmail.com>
2023-04-08 17:27:26 +0300

    GitHub Pages URL Fix Attempt 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
